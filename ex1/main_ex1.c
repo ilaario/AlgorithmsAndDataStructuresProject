b3afd608b451609e4a696c094641073598e4fed4
Dario Bonfiglio <dario.bonfiglio@edu.unito.it>
2023-03-27 16:44:27 +0200

    Creazione cartella ex1, makefile e build/main_ex1

---
+++ ex1/main_ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void*));
+
+void main(){
+    printf("Hello World!");
+}
+
+void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void*)){
+    
+}
