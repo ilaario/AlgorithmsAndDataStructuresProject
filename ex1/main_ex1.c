c100d724bbcadb60079eac1f692db61613dace52
Dario Bonfiglio <dario.bonfiglio@edu.unito.it>
2023-03-27 17:28:02 +0200

    Implementazione makefile e struttura di base di ex1

--- ex1/main_ex1.c
+++ ex1/main_ex1.c
@@ -2,10 +2,19 @@
 
 void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void*));
 
-void main(){
-    printf("Hello World!");
+
+
+int main(int argc, char *argv[]){
+    merge_binary_insertion_sort(NULL, 0, 0, atoi(argv[1]), NULL);
 }
 
 void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void*)){
-    
+    if(nitems > k){
+        printf("Merge sort\n");
+        mergeSort(base, nitems, size, compar);
+    }
+    else{
+        printf("Insertion sort\n");
+        insertionSort(base, nitems, size, compar);
+    }
 }
