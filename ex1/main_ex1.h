efbf76a44f015dd48c2b4a0845c55a0276c41acc
Dario Bonfiglio <dario.bonfiglio@edu.unito.it>
2023-04-07 15:33:09 +0200

    commit test ho perso il conto

--- ex1/main_ex1.h
+++ ex1/main_ex1.h
@@ -19,7 +19,7 @@ struct record {
     double float_f;
 };
 
-extern unsigned int length_array;
+unsigned int length_array;
 
 void merge_binary_insertion_sort(void *base, size_t nitems, size_t size, size_t k, int (*compar)(const void *, const void*));
 void insertionSort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
