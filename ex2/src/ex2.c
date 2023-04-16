43ff7a56f5c115219cfaebe1a29aa0f8ab0113b7
Dario Bonfiglio <dario.bonfiglio@edu.unito.it>
2023-04-16 13:11:06 +0200

    Commit finale EX2, mancano solo unity test

--- ex2/src/ex2.c
+++ ex2/src/ex2.c
@@ -32,7 +32,7 @@ void load_dictionary(struct SkipList* list, const char* file_name) {
         exit(EXIT_FAILURE);
     }
 
-    printf("\nLoading Dictionary...\n");
+    printf("Loading Dictionary...\n");
 
     clock_t t = clock();
     
@@ -48,7 +48,7 @@ void load_dictionary(struct SkipList* list, const char* file_name) {
     }
     t = clock() - t;
 
-    printf("\nDictionary Loaded Successfully!\n");
+    printf("Dictionary Loaded Successfully!\n");
     printf("Execution time: %f s!\n", (((float)t) / CLOCKS_PER_SEC));
 
     fclose(fp);
@@ -58,15 +58,12 @@ void check_correctme(struct SkipList* list, const char* file_name) {
     char c;
     char string[35] = "";
     FILE* fp;
-
     fp = fopen(file_name, "r");
     if (fp == NULL) {
         fprintf(stderr, "main(load_dictionary): unable to open the file\n");
         exit(EXIT_FAILURE);
     }
-
     printf("\nWords to correct:\n");
-
     clock_t t = clock();
     while ((c = (char)fgetc(fp)) != EOF) {
         if (IS_LETTER(c)) {
@@ -79,16 +76,45 @@ void check_correctme(struct SkipList* list, const char* file_name) {
             strcpy(string, "");
         }
     }
-
     if (search_skiplist(list, string) == NULL) {
         printf("%s\n", string);
     }
     strcpy(string, "");
-
     t = clock() - t;
     printf("Execution time: %f s!\n", (((float)t) / CLOCKS_PER_SEC));
-
     fclose(fp);
+}
+
+float check_correctme_test(struct SkipList* list, const char* file_name) {
+    char c;
+    char string[35] = "";
+    FILE* fp;
+    fp = fopen(file_name, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "main(load_dictionary): unable to open the file\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("\nWords to correct:\n");
+    clock_t t = clock();
+    while ((c = (char)fgetc(fp)) != EOF) {
+        if (IS_LETTER(c)) {
+            c = (char)tolower(c);
+            strncat(string, &c, 1);
+        } else if (c == ' ') {
+            if (search_skiplist(list, string) == NULL) {
+                printf("%s\n", string);
+            }
+            strcpy(string, "");
+        }
+    }
+    if (search_skiplist(list, string) == NULL) {
+        printf("%s\n", string);
+    }
+    strcpy(string, "");
+    t = clock() - t;
+    printf("Execution time: %f s!\n", (((float)t) / CLOCKS_PER_SEC));
+    fclose(fp);
+    return (((float)t) / CLOCKS_PER_SEC);
 }
 
 void find_errors(const char* path1, const char* path2, size_t max_height) {
@@ -98,21 +124,50 @@ void find_errors(const char* path1, const char* path2, size_t max_height) {
         fprintf(stderr, "find_errors(): error while creating SkipList\n");
         exit(EXIT_FAILURE);
     }
-    printf("find_errors(): SkipList created successfully!\n");
-
     load_dictionary(list, path1);
-
     check_correctme(list, path2);
-
     clear_skiplist(&list);
 }
 
+void find_errors_test(const char* path1, const char* path2) {
+    FILE *fp;
+    system("mkdir -p test");
+    fp = fopen("test/test.txt", "w");
+    for (int i = 1; i < 100; i++){
+        printf("Testing with max_height = %d\n", i);
+        struct SkipList* list;
+        new_skiplist(&list, i, compare);
+        if (list == NULL) {
+            fprintf(stderr, "find_errors(): error while creating SkipList\n");
+            exit(EXIT_FAILURE);
+        }
+        load_dictionary(list, path1);
+        float t = check_correctme_test(list, path2);
+        printf("Time: %f\n", t);
+        fprintf(fp, "%d %f\n", i, t);
+        printf("------------------------------------------------\n");
+        clear_skiplist(&list);
+    }
+    printf("Test completed!\n");
+    fclose(fp);
+}
+
 int main(int argc, char** argv){
-    if (argc < 3) {
-        fprintf(stderr, "Usage: ./main_ex2 <dictionary.txt> <correctme.txt>\n");
+
+    if (argc < 4) {
+        fprintf(stderr, "Usage: ./main_ex2 <dictionary.txt> <correctme.txt> <0 for test, x for different height>\n");
         exit(EXIT_FAILURE);
     }
-    printf("\nMAX_HEIGHT: %d\n", MAX_HEIGHT);
-    find_errors(argv[1], argv[2], MAX_HEIGHT);
-    return EXIT_SUCCESS;
+
+    int max = atoi(argv[3]);
+
+    if (max == 0) {
+        printf("\nTesting max_height with different values...\n");
+        find_errors_test(argv[1], argv[2]);
+        return EXIT_SUCCESS;
+    } else {
+        printf("MAX_HEIGHT: %d\n", max);
+        find_errors(argv[1], argv[2], max);
+        return EXIT_SUCCESS;
+    }
 }
