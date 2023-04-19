1a5d442ceaa8a416b917b1c659ddd1010e0bfad8
Dario Bonfiglio <dario.bonfiglio@edu.unito.it>
2023-04-19 15:43:44 +0200

    Ex3 Completo Co-authored-by: Inyan Fornaroli <inyan.fornaroli@di.unito.it>

---
+++ ex3-4/src/PriorityQueueJUnitTest.java
@@ -0,0 +1,7 @@
+import org.junit.runner.*;
+
+public class PriorityQueueJUnitTest {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(PriorityQueueJUnit.class);
+    }
+}
